30b72693e39e0cb9375e9acb1ccf256851ae715e
pchabbi7 <pri.17feb@gmail.com>
2023-12-01 15:22:17 -0500

    Added case to the code

--- calculation_operators.rb
+++ calculation_operators.rb
@@ -1,14 +1,27 @@
-def calculate(num1, num2)
+def calculate(num1, num2, input)
+  case input
+  when "1"
     addition = num1.to_f + num2.to_f
     puts "The first number added to second number is #{addition}"
+  when "2"
     subtraction = num1.to_f + num2.to_f
     puts "The first number subtracted to second number is #{subtraction}"
-    divide = num1.to_f / num2.to_f
+  when "4"
+    divide = num1.to_f / num2
     puts "The first number divided by second number is #{divide}"
+  when "3"
     mul = num1.to_f * num2.to_f
     puts "The first number multiplied to second number is #{mul}"
+  when "5"
     div = num1.to_f % num2.to_f
     puts "The first number mod to second number is #{div}"
+  else
+    puts "Wrong Input Entry"
+  end
+end
+
+def operation_list
+  puts "Enter 1 for ADD \nEnter 2 for Subtraction \nEnter 3 for Multiply \nEnter 4 for Divide \nEnter 4 for Mod \n "
 end
 
 puts "Simple Calculation"
@@ -20,6 +33,8 @@ num1 = gets.chomp
 puts "Enter the second number"
 num2 = gets.chomp
 
-calculate(num1, num2)
+puts "What operation do you want to perform"
+operation_list
+input = gets.chomp
 
-
+calculate(num1, num2, input)
